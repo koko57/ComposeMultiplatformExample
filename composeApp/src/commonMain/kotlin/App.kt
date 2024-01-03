cf2d16a541d012172fab0886519c4651601abef6
koko57 <aa@MacBook-Pro.local>
2024-01-03 22:06:15 +0100

    feat: display a grid of images

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,10 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -14,8 +17,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -30,4 +37,30 @@ fun App() {
             }
         Text(uiState.images.toString())
     }
+}
+
+@Composable
+fun BirdsPage(uiState: BirdsUiState) {
+    Column {
+        Row {}
+        AnimatedVisibility(visible = uiState.images.isNotEmpty()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(180.dp)
+            ) {
+                items(uiState.images) { image ->
+                    BirdImageCell(image)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        resource = asyncPainterResource("https://sebastianaigner.github.io/demo-image-api/${image.path}"),
+        contentDescription = "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth().aspectRatio(1.0f),)
+    )
 }
